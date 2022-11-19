743270d94409ab4e6d9b4b7420e77b863ddf7139
dsawws <118619951+dsawws@users.noreply.github.com>
2022-11-19 18:16:26 +0300

    Add files via upload

---
+++ родина/кино/script.js
@@ -0,0 +1,20 @@
+
+var width = 312; // ширина картинки
+var list = document.querySelector(".images");
+var pictures = document.querySelectorAll(".images2");  
+var position = 0;  
+
+document.querySelector(".prev").onclick = function() {
+    position += width; // вычисляем положение картинки
+    // рассчитываем положение последней картинки
+    position = Math.min(position, 0)
+    list.style.marginLeft = position + "px";
+};
+
+
+document.querySelector(".next").onclick = function() {
+    position -= width; // вычисляем положение картинки
+    // последнее передвижение вправо может быть не на 3, а на 2 или 1 элемент 
+    position = Math.max(position, -width * (pictures.length - 1));
+    list.style.marginLeft = position + "px";
+};
